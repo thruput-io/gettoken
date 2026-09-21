4b6104b836abe114f1bc173924e0b2a80b5d19e3
Agent Tore <tore@Johans-MacBook-Air.local>
2026-09-21 21:12:47 +0200

    config: extract shared variables into config.env

--- config.linux.mk
+++ config.linux.mk
@@ -1,4 +1,4 @@
 PACKAGE_FORMATS = deb
 
-INSTALL_COMMAND = apt-get update && apt-get install -y --no-install-recommends
+INSTALL_COMMAND = $(subst ",,$(DEBIAN_INSTALL_COMMAND))
 BUILD_DEPS = jq bats shellcheck golang-go kcov gnupg xmlstarlet dpkg-dev debhelper lintian build-essential golang-any apt-utils git curl ca-certificates perl
--- config.mk
+++ config.mk
@@ -1,4 +1,7 @@
+-include config.env
+
 BRANCH = $(shell git rev-parse --abbrev-ref HEAD)
+SITE_URL = $(subst ",,$(PROD_SITE_URL))
 
 -include config.$(shell uname -s | tr '[:upper:]' '[:lower:]').mk
 -include config.local.mk
