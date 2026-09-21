7885ab1b7c926d68ef0f3efdef3473bf64b3fbee
Agent Tore <tore@Johans-MacBook-Air.local>
2026-09-21 20:19:19 +0200

    config: remove default SITE_URL from config.mk

--- config.mk
+++ config.mk
@@ -1,5 +1,4 @@
 BRANCH = $(shell git rev-parse --abbrev-ref HEAD)
-SITE_URL = https://thruput.se/gettoken
 
 -include config.$(shell uname -s | tr '[:upper:]' '[:lower:]').mk
 -include config.local.mk
