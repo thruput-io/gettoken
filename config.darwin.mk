bdf8defba507bbe7a1087905f12b40fded03848f
Agent Tore <tore@Johans-MacBook-Air.local>
2026-09-21 21:15:56 +0200

    config: remove quotes from config.env and subst from config.mk

--- config.darwin.mk
+++ config.darwin.mk
@@ -1,4 +1,4 @@
 PACKAGE_FORMATS = brew
 
-INSTALL_COMMAND = $(subst ",,$(MAC_INSTALL_COMMAND))
+INSTALL_COMMAND = $(MAC_INSTALL_COMMAND)
 BUILD_DEPS = jq bats-core shellcheck go kcov bash gnupg xmlstarlet
--- config.linux.mk
+++ config.linux.mk
@@ -1,4 +1,4 @@
 PACKAGE_FORMATS = deb
 
-INSTALL_COMMAND = $(subst ",,$(DEBIAN_INSTALL_COMMAND))
+INSTALL_COMMAND = $(DEBIAN_INSTALL_COMMAND)
 BUILD_DEPS = jq bats shellcheck golang-go kcov gnupg xmlstarlet dpkg-dev debhelper lintian build-essential golang-any apt-utils git curl ca-certificates perl
