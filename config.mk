28e3c0824f583006e17a11ce8216391e1ff66b42
Agent Tore <tore@Johans-MacBook-Air.local>
2026-09-21 20:17:28 +0200

    config: add SITE_URL default to config.mk

--- config.mk
+++ config.mk
@@ -1,4 +1,5 @@
 BRANCH = $(shell git rev-parse --abbrev-ref HEAD)
+SITE_URL = https://thruput.se/gettoken
 
 -include config.$(shell uname -s | tr '[:upper:]' '[:lower:]').mk
 -include config.local.mk
