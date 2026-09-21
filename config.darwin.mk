9ab90e7d28403ea5f85dd1256444d93fee01cc1a
Agent Tore <tore@Johans-MacBook-Air.local>
2026-09-21 21:32:05 +0200

    config: use localenv.sh and env-based variables in verifications.yml

--- config.darwin.mk
+++ config.darwin.mk
@@ -1,4 +1,5 @@
 PACKAGE_FORMATS = brew
 
-INSTALL_COMMAND = $(MAC_INSTALL_COMMAND)
+MAC_INSTALL_COMMAND ?= brew install --no-ask
+INSTALL_COMMAND ?= $(MAC_INSTALL_COMMAND)
 BUILD_DEPS = jq bats-core shellcheck go kcov bash gnupg xmlstarlet
